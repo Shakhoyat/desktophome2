a4244f8a097abd56bc3219361d22659351305823
Shakhoyat <shujon2107104@stud.kuet.ac.bd>
2024-11-09 22:26:04 +0600

    learning using vbox

--- src/main/java/com/example/demo/home2/HelloApplication.java
+++ src/main/java/com/example/demo/home2/HelloApplication.java
@@ -12,49 +12,22 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class HelloApplication extends Application {
-    private static void handle(ActionEvent event) {
-        System.out.println("UI is now Blocked");
-        try {
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public void init() throws Exception {
-        super.init();
-        System.out.println("Before the file starts");
-        System.out.println(Thread.currentThread().getName());
-        Thread.sleep(5000);
-        System.out.println("After the file starts");
-    }
     @Override
     public void start(Stage stage)  {
-        Platform.setImplicitExit(false);
-        System.out.println(Thread.currentThread().getName());
         VBox root = new VBox();
-        root.setAlignment(Pos.CENTER);
-        root.setPadding(new Insets(25,25,20,20));
-        root.setSpacing(10);
-        Label text=new Label("Hello World");
-        Button button=new Button("Click me");
-        button.setOnAction(event ->text.setText("Welcome on Board"));
-        Button button2=new Button("Blocked");
-        button2.setOnAction(HelloApplication::handle);
-        Button button1=new Button("Exit explicitly");
-        button1.setOnAction(event -> Platform.exit());
-        root.getChildren().addAll(text,button,button1,button2);
-        Scene scene=new Scene(root,350,300);
+       root.setAlignment(Pos.CENTER);
+       root.setSpacing(10);
+       Label label = new Label("Click the Button");
+       Button button = new Button("Click me");
+       button.setPrefWidth(100);
+       button.setPadding(new Insets(10, 10, 10, 40));
+       root.getChildren().addAll(label, button);
+       Scene scene = new Scene(root,500,450);
         stage.setScene(scene);
-        stage.setTitle("Hello StageTitle");
+        stage.setTitle("Event");
         stage.show();
  }
-// @Override
-// public void stop() throws Exception {
-//     System.out.println(Thread.currentThread().getName());
-//     System.out.println("After closing the file ");
-// }
+
  public static void main(String[] args) {launch();}
 
 }
