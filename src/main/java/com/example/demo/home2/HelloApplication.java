eebeb4a9776234e4af5a4f644e1c37cc7be17683
Shakhoyat <shujon2107104@stud.kuet.ac.bd>
2024-11-09 11:28:42 +0600

    commit2

--- src/main/java/com/example/demo/home2/HelloApplication.java
+++ src/main/java/com/example/demo/home2/HelloApplication.java
@@ -1,23 +1,30 @@
 package com.example.demo.home2;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("HelloWorld!");
+    public void start(Stage stage)  {
+        VBox root = new VBox();
+        root.setAlignment(Pos.CENTER);
+        root.setPadding(new Insets(25,25,20,20));
+        root.setSpacing(10);
+        Label text=new Label("Hello World");
+        Button button=new Button("Click me");
+        button.setOnAction(event ->text.setText("Welcome on Board"));
+        root.getChildren().addAll(text,button);
+        Scene scene=new Scene(root,350,300);
         stage.setScene(scene);
+        stage.setTitle("Hello StageTitle");
         stage.show();
-    }
-
-    public static void main(String[] args) {
-        launch();
-    }
+ } public static void main(String[] args) {
+launch();
+}
 }
